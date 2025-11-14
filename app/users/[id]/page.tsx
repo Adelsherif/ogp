22167fe09818bed4eedbe050c04d30d633245273
Adel Sherif <adeladeladel515151@gmail.com>
2025-11-14 20:05:40 +0200

    first commit

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -57,7 +57,7 @@ export default async function ProductPage({ params }: { params: Params }) {
       <img src={product.image} alt={product.name} width={300} height={300} />
 <WhatsAppShareButton
   id={product.id.toString()}
-  baseUrl={process.env.NEXT_PUBLIC_BASE_URL || ''}
+  baseUrl={process.env.NEXT_PUBLIC_BASE_URL || 'https://ogp-git-master-adelsherifs-projects.vercel.app/'}
 />
 
     </div>
